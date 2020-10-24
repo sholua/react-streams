68d26d86943d0d5264f064d1774803815995b549
Oleksandr Shevchuk <s471652@gmail.com>
2020-10-24 09:40:39 +0300

    Navigation with react-router-dom

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import StreamList from "./streams/StreamList";
+import StreamCreate from "./streams/StreamCreate";
+import StreamShow from "./streams/StreamShow";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import Header from "./Header";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Route exact path="/" component={StreamList} />
+        <Route exact path="/streams/new" component={StreamCreate} />
+        <Route exact path="/streams/show" component={StreamShow} />
+        <Route exact path="/streams/edit" component={StreamEdit} />
+        <Route exact path="/streams/delete" component={StreamDelete} />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
