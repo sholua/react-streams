8c112708f52930c49496048325d24c55fefb9a1c
Oleksandr Shevchuk <s471652@gmail.com>
2020-10-25 18:38:25 +0200

    Programmatic navigation in action creator

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -9,6 +9,7 @@ import {
 } from "./types";
 
 import streams from "../apis/streams";
+import history from "../history";
 
 export const signIn = (userId) => ({
   type: SIGN_IN,
@@ -24,6 +25,7 @@ export const createStream = (formValues) => async (dispatch, getState) => {
   const response = await streams.post("/streams", { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  history.push("/");
 };
 
 export const fetchStreams = () => async (dispatch) => {
