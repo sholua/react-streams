39d24062f5d9f5ffb06869d929f63f69d947836a
Oleksandr Shevchuk <s471652@gmail.com>
2020-10-26 11:45:00 +0200

    Modal for deleting stream
    Rendering model with ReactDOM.createPortal()

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
+import Modal from "../Modal";
+import history from "../../history";
 
-const StreamDelete = () => {
-  return <div>StreamDelete</div>;
-};
+class StreamDelete extends Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
 
-export default StreamDelete;
+  renderActions() {
+    return (
+      <React.Fragment>
+        <button className="ui button">Cancel</button>
+        <button className="ui button negative">Delete</button>
+      </React.Fragment>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        StreamDelete
+        <Modal
+          title="Delete a stream"
+          content="Are you sure you wanna delete this stream?"
+          actions={this.renderActions()}
+          onDismiss={() => history.push("/")}
+        />
+      </div>
+    );
+  }
+}
+
+export default connect(null, { fetchStream })(StreamDelete);
